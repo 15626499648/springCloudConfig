683b10a0f4ec1e7d92f7d3b6249f67c1139541a3
15626499648 <645359637@qq.com>
2019-01-03 19:00:50 +0800

    RestTemplate+Ribbon

--- spring-cloud-config/config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ spring-cloud-config/config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -21,9 +22,17 @@ public class ConfigClientApplication {
 
     @Value("${foo}")
     String foo;
+
+    @Value("${server.port}")
+    String port;
+
     @RequestMapping(value = "/hi")
-    public String hi(){
+    public String hi() {
         return foo;
     }
 
+    @RequestMapping("/hello")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
 }
