2487bb774dedcd0e21a683482dc2bd215bd09d1a
15626499648 <645359637@qq.com>
2019-01-03 19:25:06 +0800

    使用Feign消费服务。

---
+++ spring-cloud-config/config-feign/src/main/java/com/example/demo/service/SchedualServiceHi.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "CONFIG-CLIENT")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
