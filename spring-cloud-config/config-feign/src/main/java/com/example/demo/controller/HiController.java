995e9912bbfa25bf5ccfefd168566c963058d2f2
cja <[645359637@qq.com]>
2019-01-06 00:03:55 +0800

    加入订单服务，config-feign服务配置hystrix、ribbon超时调试

--- spring-cloud-config/config-feign/src/main/java/com/example/demo/controller/HiController.java
+++ spring-cloud-config/config-feign/src/main/java/com/example/demo/controller/HiController.java
@@ -1,16 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.common.domain.Order;
 import com.example.demo.service.SchedualServiceHi;
 
+import com.example.orderapi.service.OrderApiService;
 import com.example.product.service.ProductApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HiController {
-
 
     //编译器报错，无视。 因为这个Bean是在程序启动的时候注入的，编译器感知不到，所以报错。
     @Autowired
@@ -18,6 +20,9 @@ public class HiController {
 
     @Autowired
     public ProductApiService productApiService;
+
+    @Autowired
+    public OrderApiService orderApiService;
 
     @GetMapping(value = "/hi")
     public String sayHi(@RequestParam String name) {
@@ -27,6 +32,16 @@ public class HiController {
     @GetMapping(value = "/hello")
     public String sayHello(@RequestParam String name) {
         return productApiService.test(name);
+    }
+
+    @GetMapping(value = "/testAdd/{productId}/{amount}")
+    public String testAdd(@PathVariable Long productId, @PathVariable int amount) {
+        Order order = new Order();
+        order.setProductId(productId);
+        order.setAmount(amount);
+        int row = orderApiService.addOrder(order);
+        System.out.println(row);
+        return "成功";
     }
 
 }
