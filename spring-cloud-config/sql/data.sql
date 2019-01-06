27248d053cd65bc3e93a1237a59571bdb83acadd
cja <[645359637@qq.com]>
2019-01-06 21:43:42 +0800

    sql文件

---
+++ spring-cloud-config/sql/data.sql
@@ -0,0 +1,55 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.6.5-m8 : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `test`;
+
+/*Table structure for table `t_order` */
+
+DROP TABLE IF EXISTS `t_order`;
+
+CREATE TABLE `t_order` (
+  `id` bigint(20) NOT NULL,
+  `order_number` varchar(20) DEFAULT NULL,
+  `product_id` bigint(20) DEFAULT NULL,
+  `amount` int(10) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `t_order` */
+
+insert  into `t_order`(`id`,`order_number`,`product_id`,`amount`,`create_time`) values (154670309252108,'1000000164477267',154667039359483,1,'2019-01-05 23:44:53'),(154670309356876,'1000000959361646',154667039359483,1,'2019-01-05 23:44:54');
+
+/*Table structure for table `t_product` */
+
+DROP TABLE IF EXISTS `t_product`;
+
+CREATE TABLE `t_product` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `stock` int(10) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `t_product` */
+
+insert  into `t_product`(`id`,`name`,`stock`,`create_time`) values (154667039359483,'电脑2',20,'2019-01-05 14:39:54'),(154667050167257,'手机',20,'2019-01-05 14:41:42');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
